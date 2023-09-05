85e94630a4b7c0fd60ab1b2d839e361fb7740ccd
Reubi0910 <esteban.morillo@divtec.ch>
2023-09-05 15:00:31 +0200

    tests if the input is a number before continuing

--- js/main.js
+++ js/main.js
@@ -10,8 +10,14 @@ let a = 'Bonjour';
 
 console.log(a);
 
+// entrée du nombre en celcius par l'utilisateur
 let tempC = prompt("Température en celcius :");
 
-let tempF = tempC * 9 / 5  + 32;
-
-alert(`${tempC}°C = ${tempF}°F`);
+// testes si la valeur entrée est un nombre
+if(isNaN(tempC)) {
+    alert("Température Invalide !")
+} else {
+    // calcule et affiche la température en fahrenheit
+    let tempF = tempC * 9 / 5  + 32;
+    alert(`${tempC}°C = ${tempF}°F`);
+}
